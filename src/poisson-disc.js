f026189b078d5d29f450dbf3610f0c9930e40689
Andrey V <avelichk@uwo.ca>
2025-03-03 22:54:47 -0500

    Added rudimentary visualization, point generation

--- src/poisson-disc.js
+++ src/poisson-disc.js
@@ -7,8 +7,8 @@ function PoissonDisc(radius, gridSize, maxTries = 20) {
     const outerCircle = radius * 2;
 
     const canvas = document.getElementById("myCanvas");
-    canvas.width = 100;
-    canvas.height = 100;
+    canvas.width = x;
+    canvas.height = y;
     const ctx = canvas.getContext("2d");
 
 
@@ -30,35 +30,54 @@ function PoissonDisc(radius, gridSize, maxTries = 20) {
         // Retrieve the actual point at the index
         spawnCenter = spawnedPoints[spawnedPointIndex]; 
         console.log(spawnCenter);
-        drawPoint(ctx, spawnCenter[0], spawnCenter[1]); 
+        drawPoint(ctx, spawnCenter[0], spawnCenter[1], "red"); 
+        drawCircle(ctx, spawnCenter[0], spawnCenter[1], outerCircle);
 
         for(i = 0; i < maxTries; i++) {
-            // Generates a random angle in radians at which to draw the point
+            // Explanation 1)
             let angle = Math.random() * Math.PI*2;
-
-            // We take cos: the horizontal distance, sin : the vertical distance of the angle and scale by the center point,
-            // in order to get the position of the absolute point on the grid.
-            angleX = spawnCenter[0] + Math.cos(angle)*radius;
-            angleY = spawnCenter[1] + Math.sin(angle)*radius;
+            randomScaler = Math.random() * (outerCircle - radius) + radius;
+            
+            angleX = spawnCenter[0] + Math.cos(angle) * randomScaler;
+            angleY = spawnCenter[1] + Math.sin(angle) * randomScaler;
 
             const neighbourPoint = [angleX, angleY];
-            drawPoint(ctx, neighbourPoint[0], neighbourPoint[1]);
-            console.log("x: %d, y: %d", angleX, angleY);
+            console.log(neighbourPoint);
+            drawPoint(ctx, neighbourPoint[0], neighbourPoint[1], "white");
 
-            
-        }
+            if(isValid) {
+                finalizedPoints.push(neighbourPoint);
+                spawnedPoints.push(neighbourPoint);
+            }
 
         return;
 
+        }
     }
 }
 
-function drawPoint(ctx, x, y) {
+
+function isValid() {
+
+}
+
+function drawPoint(ctx, x, y, color) {
     ctx.beginPath();
     ctx.moveTo(x, y);
     ctx.arc(x, y, 1, 0, 2 * Math.PI, true);
-    ctx.fillStyle = "white";
+    ctx.fillStyle = color;
     ctx.fill();
+    ctx.closePath();
+}
+
+function drawCircle(ctx, x, y, radius) {
+    ctx.beginPath();
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.arc(x, y, radius, 0, 2 * Math.PI, true);
+    ctx.strokeStyle = 'red';        // Set the outline color (optional)
+    ctx.lineWidth = 1;              // Set the outline width (optional)
+    ctx.stroke(); 
     ctx.closePath();
 }
 
